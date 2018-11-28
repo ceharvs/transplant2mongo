f41b6d22b7a79635da3a0a7bb715f77efe768ca5
Christine Harvey <ceharvey@mitre.org>
2018-11-28 08:22:54 -0600

    Converted to meet PEP 8 standards and Python3

--- test_database.py
+++ test_database.py
@@ -6,10 +6,10 @@ Input Parameters:
 - client: Location of the database where the files and information
     are being stored in MongoDB.
 - database: Location of the database where the files and information
-	are being stored in MongoDB.
+    are being stored in MongoDB.
 - collection_name: This is the name of the parent collection for the
-	data.  For example, if we want to pull from the Living Donors
-	collection then the collection_name would be Living_Donor
+    data.  For example, if we want to pull from the Living Donors
+    collection then the collection_name would be Living_Donor
 - file_name: Name of file to save the aggregated data to
 - variable: Variable to search on
 - value: Value of the variable to return
@@ -22,11 +22,11 @@ import argparse
 parser = argparse.ArgumentParser(description='Process command line input')
 parser.add_argument('client', help='Location of the database where the files '
                                    'and information are being stored in MongoDB.',
-	type=str)
+    type=str)
 parser.add_argument('database', help='Database that will be used to store the data',
-	type=str)
+    type=str)
 parser.add_argument('collection_name', help='Name of the collection to be used',
-	type=str)
+    type=str)
 parser.add_argument('variable', help='Variables to search on', 
     type=str)
 parser.add_argument('value', help='Value for the variable', 
@@ -40,11 +40,11 @@ collection = db[args.collection_name]
 
 # Try to convert the value to an int
 try:
-  value = int(args.value)
+    value = int(args.value)
 except ValueError as verr:
-  pass # do job to handle: s does not contain anything convertible to int
+    pass  # do job to handle: s does not contain anything convertible to int
 except Exception as ex:
-  pass # do job to handle: Exception occurred while converting to int
+    pass  # do job to handle: Exception occurred while converting to int
 
-print collection.find_one({args.variable: value})
+print(collection.find_one({args.variable: value}))
 
