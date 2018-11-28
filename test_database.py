5e9b5f6bc3b6c60953aa1291de375fdd08255f05
Christine Harvey <ceharvey@mitre.org>
2018-11-28 17:22:03 -0500

    Cosmetic updates

--- test_database.py
+++ test_database.py
@@ -21,30 +21,19 @@ import argparse
 # Parse in the input arguments
 parser = argparse.ArgumentParser(description='Process command line input')
 parser.add_argument('client', help='Location of the database where the files '
-                                   'and information are being stored in MongoDB.',
-    type=str)
-parser.add_argument('database', help='Database that will be used to store the data',
-    type=str)
-parser.add_argument('collection_name', help='Name of the collection to be used',
-    type=str)
-parser.add_argument('variable', help='Variables to search on', 
-    type=str)
-parser.add_argument('value', help='Value for the variable', 
-    type=str)
-args=parser.parse_args()
+                                   'and information are being stored in MongoDB.')
+parser.add_argument('database', help='Database that will be used to store the data')
+parser.add_argument('collection_name', help='Name of the collection to be used')
+parser.add_argument('variable', help='Variables to search on')
+parser.add_argument('value', help='Value for the variable')
+args = parser.parse_args()
 
 
 # Establish a database connection
 db = pymongo.MongoClient(args.client)[args.database]
 collection = db[args.collection_name]
 
-# Try to convert the value to an int
-try:
-    value = int(args.value)
-except ValueError as verr:
-    pass  # do job to handle: s does not contain anything convertible to int
-except Exception as ex:
-    pass  # do job to handle: Exception occurred while converting to int
-
+# Print the output
+value = str(args.value)
 print(collection.find_one({args.variable: value}))
 
